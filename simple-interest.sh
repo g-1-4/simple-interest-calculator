dc2a7e39e25fd3428bee0e6bbdb391239aecee8e
Gowtham Sriram <112844503+g-1-4@users.noreply.github.com>
2025-07-23 19:21:56 +0530

    added bash file

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# simple-interest.sh
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of Interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+echo "Simple Interest = $si"
+echo "Total Amount = $(echo "scale=2; $p + $si" | bc)"
+echo "Thank you for using the Simple Interest Calculator!"
